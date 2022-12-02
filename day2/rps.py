26b99b4b62bfb48698d8baf83c096d26ce4cf25a
Landon Smith <lcsmith2.0@gmail.com>
2022-12-01 23:20:17 -0600

    Solve day 2 part 1

---
+++ day2/rps.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+
+def run_day2():
+    with open(os.path.join(sys.path[0], "day2\\input"), "r") as strategy:
+        lines = strategy.readlines()
+
+    total_score = 0
+    for line in lines:
+        plan = line.split()
+        opponent = convert_opponent(plan[0])
+        thrown = convert_thrown(plan[1])
+        total_score += convert_thrown(plan[1])
+        total_score += score_outcome(opponent, thrown)
+
+    print(total_score)
+
+
+def convert_thrown(thrown):
+    match thrown:
+        case "X":
+            return 1
+        case "Y":
+            return 2
+        case "Z":
+            return 3
+
+
+def convert_opponent(opponent):
+    match opponent:
+        case "A":
+            return 1
+        case "B":
+            return 2
+        case "C":
+            return 3
+
+
+def score_outcome(opponent, thrown):
+    if thrown == opponent:
+        return 3
+    if thrown == (opponent % 3)+1:
+        return 6
+    return 0
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from day1.elf_calories import run_day1
+from day2.rps import run_day2
 
 if __name__ == '__main__':
-    run_day1(3)
+    run_day2()
 
