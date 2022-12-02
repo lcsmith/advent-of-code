5b129e0d5a548e8a369a6ff01e6ce6d54fcf2cd4
Landon Smith <lcsmith2.0@gmail.com>
2022-12-01 22:09:38 -0600

    Add day 1 solution. Hello

---
+++ day1/elf_calories.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def run_day1(num_elves):
+    with open(os.path.join(sys.path[0], "day1\\input"), "r") as elves:
+        lines = elves.readlines()
+
+    best_calories = [0]*num_elves
+    current_calories = 0
+
+    for line in lines:
+        if line.isspace():
+            if current_calories > best_calories[0]:
+                best_calories[0] = current_calories
+                best_calories.sort()
+            current_calories = 0
+        else:
+            current_calories += int(line)
+
+    print(sum(best_calories))
---
+++ main.py
@@ -0,0 +1,5 @@
+from day1.elf_calories import run_day1
+
+if __name__ == '__main__':
+    run_day1(3)
+
