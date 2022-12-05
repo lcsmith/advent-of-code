8484154d4352695f36289b6cb0e76062e48bc705
Landon Smith <lcsmith2.0@gmail.com>
2022-12-04 23:58:35 -0600

    Solve 5.2 (this seems hacky but ok)

--- 2022/day5/cratestack.py
+++ 2022/day5/cratestack.py
@@ -3,18 +3,26 @@ import sys
 
 
 def run_day5_part1():
+    return run_with_movement_handler(one_at_a_time)
+
+
+def run_day5_part2():
+    return run_with_movement_handler(all_at_once)
+
+
+def run_with_movement_handler(crane):
     with open(os.path.join(sys.path[0], "day5\\input"), "r") as infile:
         lines = infile.readlines()
 
         crates = [[] for _ in range(9)]
         handler = handle_initial_input
         for line in lines:
-            handler = handler(line, crates)
+            handler = handler(line, crates, crane)
 
         return [crate[0] for crate in crates]
 
 
-def handle_initial_input(line, crates):
+def handle_initial_input(line, crates, _):
     if line[1] == "1":
         return handle_movement
     crate_index = 1
@@ -26,15 +34,26 @@ def handle_initial_input(line, crates):
     return handle_initial_input
 
 
-def handle_movement(line, crates):
+def handle_movement(line, crates, crane):
     if line == "\n":
         return handle_movement
     instructions = line.split()
     num_to_move = int(instructions[1])
-    source = int(instructions[3])-1
-    destination = int(instructions[5])-1
+    source_index = int(instructions[3])-1
+    destination_index = int(instructions[5])-1
 
-    for _ in range(num_to_move):
-        crate = crates[source].pop(0)
-        crates[destination].insert(0, crate)
+    crane(num_to_move, crates[source_index], crates[destination_index])
+
     return handle_movement
+
+
+def one_at_a_time(num_to_move, source, destination):
+    for _ in range(num_to_move):
+        crate = source.pop(0)
+        destination.insert(0, crate)
+
+
+def all_at_once(num_to_move, source, destination):
+    for index in range(num_to_move):
+        crate = source.pop(0)
+        destination.insert(index, crate)
--- 2022/main.py
+++ 2022/main.py
@@ -2,7 +2,7 @@ from day1.calories import run_day1
 from day2.rps import run_day2_part2, run_day2_part1
 from day3.rucksack import run_day3_part1, run_day3_part2
 from day4.cleaning import run_day4_part1, run_day4_part2
-from day5.cratestack import run_day5_part1
+from day5.cratestack import run_day5_part1, run_day5_part2
 
 
 def run(day, part):
@@ -25,7 +25,9 @@ def run(day, part):
             return run_day4_part2()
         case 5, 1:
             return run_day5_part1()
+        case 5, 2:
+            return run_day5_part2()
 
 
 if __name__ == '__main__':
-    print(run(5, 1))
+    print(run(5, 2))
