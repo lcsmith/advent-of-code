8332ccb7e8938a0713980d55f85f626b01728fcb
Landon Smith <lcsmith2.0@gmail.com>
2022-12-05 23:16:47 -0600

    Solve 6.2 (it's cleaner this way anyways)

--- 2022/day6/signal.py
+++ 2022/day6/signal.py
@@ -3,19 +3,15 @@ import sys
 import numpy
 
 
-def run_day6_part1():
+def run_day6(num_distinct):
     with open(os.path.join(sys.path[0], "day6\\input"), "r") as infile:
         signal = infile.readline()
 
-        chars = [signal[0], signal[1], signal[2], signal[3]]
-        for char_index in range(len(signal)):
-            chars[char_index % 4] = signal[char_index]
-            if len(numpy.unique(chars)) == 4:
+        chars = [signal[i] for i in range(num_distinct)]
+        for char_index in range(num_distinct, len(signal)):
+            chars[char_index % num_distinct] = signal[char_index]
+            if len(numpy.unique(chars)) == num_distinct:
                 return char_index+1
 
     return -1
-
-
-def run_day6_part2():
-    return
 
--- 2022/main.py
+++ 2022/main.py
@@ -3,7 +3,7 @@ from day2.rps import run_day2_part2, run_day2_part1
 from day3.rucksack import run_day3_part1, run_day3_part2
 from day4.cleaning import run_day4_part1, run_day4_part2
 from day5.cratestack import run_day5_part1, run_day5_part2
-from day6.signal import run_day6_part1, run_day6_part2
+from day6.signal import run_day6
 
 
 def run(day, part):
@@ -29,8 +29,10 @@ def run(day, part):
         case 5, 2:
             return run_day5_part2()
         case 6, 1:
-            return run_day6_part1()
+            return run_day6(4)
+        case 6, 2:
+            return run_day6(14)
 
 
 if __name__ == '__main__':
-    print(run(6, 1))
+    print(run(6, 2))
