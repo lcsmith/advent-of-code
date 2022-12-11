82a37f606cb4810d4fc62e863a1ee2a6a8fdf229
Landon Smith <lcsmith2.0@gmail.com>
2022-12-11 09:20:17 -0600

    Cleanliness is next to monkeyness

--- 2022/day11/monke.py
+++ 2022/day11/monke.py
@@ -4,6 +4,31 @@ import math
 
 
 def run():
+    is_part2 = True
+    monkeys = parse_monkeys()
+
+    worry_modulation = math.prod(map(lambda m: m.test_divisor, monkeys.values()))
+    for game_round in range(10000):
+        for monkey in monkeys.values():
+            for item in monkey.items:
+                item = monkey.operation(item)
+                if is_part2:
+                    item = item % worry_modulation
+                else:
+                    item = math.floor(item / 3)
+                if (item % monkey.test_divisor) == 0:
+                    monkeys[monkey.true_throw].items.append(item)
+                else:
+                    monkeys[monkey.false_throw].items.append(item)
+            monkey.inspections += len(monkey.items)
+            monkey.items.clear()
+
+    monkey_list = list(monkeys.values())
+    monkey_list.sort(key=lambda m: m.inspections, reverse=True)
+    print(monkey_list[0].inspections * monkey_list[1].inspections)
+
+
+def parse_monkeys():
     with open(os.path.join(sys.path[0], "input"), "r") as infile:
         lines = infile.readlines()
 
@@ -23,35 +48,16 @@ def run():
             if line == "  Operation: new = old * old\n":
                 current_monkey.operation = lambda x: x * x
             elif words[4] == "*":
-                operand = int(words[5])
-                current_monkey.operation = lambda x, operand = operand: x * operand
+                current_monkey.operation = lambda x, operand = int(words[5]): x * operand
             elif words[4] == "+":
-                operand = int(words[5])
-                current_monkey.operation = lambda x, operand = operand: x + operand
+                current_monkey.operation = lambda x, operand = int(words[5]): x + operand
         elif words[0] == "Test:":
             current_monkey.test_divisor = int(words[3])
-        elif words[0:2] == ["If", "true:"]:
+        elif words[:2] == ["If", "true:"]:
             current_monkey.true_throw = int(words[5])
-        elif words[0:2] == ["If", "false:"]:
+        elif words[:2] == ["If", "false:"]:
             current_monkey.false_throw = int(words[5])
-
-    worry_modulation = math.prod(map(lambda m: m.test_divisor, monkeys.values()))
-    for game_round in range(10000):
-        for monkey_index in range(len(monkeys)):
-            monkey = monkeys[monkey_index]
-            for item in monkey.items:
-                item = monkey.operation(item)
-                #item = math.floor(item / 3) ## from part 1
-                item = item % worry_modulation
-                if (item % monkey.test_divisor) == 0:
-                    monkeys[monkey.true_throw].items.append(item)
-                else:
-                    monkeys[monkey.false_throw].items.append(item)
-            monkey.inspections += len(monkey.items)
-            monkey.items.clear()
-    monkey_list = list(monkeys.values())
-    monkey_list.sort(key=lambda m: m.inspections, reverse=True)
-    print(monkey_list[0].inspections * monkey_list[1].inspections)
+    return monkeys
 
 
 class Monkey:
