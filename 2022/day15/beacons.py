10714bf5c1bc56c61d126bfa2578a9d18580c7a2
Landon Smith <lcsmith2.0@gmail.com>
2022-12-15 00:13:35 -0600

    And an even messier part 2
    Janky, I'll clean this up tomorrow

--- 2022/day15/beacons.py
+++ 2022/day15/beacons.py
@@ -2,7 +2,7 @@ import re
 import math
 
 
-def run():
+def run_part1():
     with open('input') as infile:
         lines = [line.strip() for line in infile]
     exclusions = set([])
@@ -13,16 +13,64 @@ def run():
         match = re.findall("Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", line)[0]
         sensor = (int(match[0]), int(match[1]))
         beacon = (int(match[2]), int(match[3]))
-        distance = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+        distance = get_distance(sensor, beacon)
         for x in range(sensor[0] - distance, sensor[0] + distance):
             if get_distance(sensor, (x, important_y)) <= distance and (x, important_y) != beacon:
                 exclusions.add((x, important_y))
     print(len(exclusions))
 
 
-def get_distance(pointA, pointB):
-    return abs(pointA[0] - pointB[0]) + abs(pointA[1] - pointB[1])
+def run_part2():
+    with open('input') as infile:
+        lines = [line.strip() for line in infile]
+    sensor_distances = []
+    for line in lines:
+        if not line:
+            continue
+        match = re.findall("Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", line)[0]
+        sensor = (int(match[0]), int(match[1]))
+        beacon = (int(match[2]), int(match[3]))
+        distance = get_distance(sensor, beacon)
+        sensor_distances.append((sensor, distance))
+
+    for y in range(0, 4000001):
+        inclusions = [(0, 4000001)]
+        exclusions = []
+        for sensor_distance in sensor_distances:
+            x_range = get_x_range(*sensor_distance, y)
+            if x_range:
+                exclusions.append(x_range)
+        exclusions.sort()
+        for exclusion in exclusions:
+            for inclusion in inclusions.copy():
+                if exclusion[0] <= inclusion[0] <= exclusion[1] <= inclusion[1]:
+                    inclusions.remove(inclusion)
+                    inclusions.append((exclusion[1]+1, inclusion[1]))
+                elif inclusion[0] <= exclusion[0] <= inclusion[1] <= exclusion[1]:
+                    inclusions.remove(inclusion)
+                    inclusions.append((inclusion[0], exclusion[0]-1))
+                elif inclusion[0] <= exclusion[0] <= exclusion[1] <= inclusion[1]:
+                    inclusions.remove(inclusion)
+                    inclusions.append((inclusion[0], exclusion[0]-1))
+                    inclusions.append((exclusion[1]+1, inclusion[1]))
+                elif exclusion[0] <= inclusion[0] <= inclusion[1] <= exclusion[1]:
+                    inclusions.remove(inclusion)
+        if inclusions:
+            print(inclusions)
+            print(y)
+
+
+def get_x_range(sensor, distance, y):
+    y_distance = abs(sensor[1] - y)
+    remaining_distance = distance - y_distance
+    if remaining_distance < 0:
+        return None
+    return sensor[0] - remaining_distance, sensor[0] + remaining_distance
+
+
+def get_distance(point_a, point_b):
+    return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
 
 
 if __name__ == '__main__':
-    run()
+    run_part2()
