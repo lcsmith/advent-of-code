824fa719bae87729d13efa36d30c30b322154b48
Landon Smith <lcsmith2.0@gmail.com>
2022-12-16 21:11:15 -0600

    Solve part 2. It's not jank, it's HCI

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -1,11 +1,54 @@
 import itertools
 import re
-from functools import cmp_to_key
-
+import matplotlib.pyplot as plt
 import networkx as nx
 
 
 def run():
+    valves, flows = parse_graph()
+
+    visualize = False
+    if visualize:
+        nx.draw(valves, with_labels=True)
+        plt.show()
+        return
+
+    shortest_paths = dict(nx.all_pairs_shortest_path(valves))
+    unopened = [valve for valve in flows.keys() if flows[valve] != 0]
+
+    unopened_1 = ['KL', 'YS', 'CD', 'ID', 'AC', 'MJ', 'BO', 'KW', 'DT']
+    unopened_2 = ['OU', 'DY', 'RH', 'DI', 'TO', 'OI']
+
+    print(get_best(shortest_paths, flows, unopened, 30, 7))
+    print(get_best(shortest_paths, flows, unopened_1, 26, len(unopened_1)) +
+          get_best(shortest_paths, flows, unopened_2, 26, len(unopened_2)))
+
+
+def get_best(shortest_paths, flows, unopened, total_time, choose):
+    best = 0
+    for permute in itertools.permutations(unopened, choose):
+        current = "AA"
+        total_flow = 0
+        time = 0
+        my_permute = list(permute)
+        my_unopened = list(unopened)
+        while time < total_time:
+            if current in my_unopened:
+                my_unopened.remove(current)
+                time = time + 1
+                total_flow = total_flow + (total_time - time) * flows[current]
+            elif my_unopened and my_permute:
+                target = my_permute.pop()
+                distance = len(shortest_paths[current][target]) - 1
+                time = time + distance
+                current = target
+            else:
+                time = time + 1
+        best = max(best, total_flow)
+    return best
+
+
+def parse_graph():
     with open('input') as infile:
         lines = [line.strip() for line in infile]
 
@@ -22,33 +65,7 @@ def run():
         valves.add_node(valve)
         for connection in connections:
             valves.add_edge(valve, connection.strip())
-    shortest_paths = dict(nx.all_pairs_shortest_path(valves))
-    unopened = list(flows.keys())
-    for unopen in list(unopened):
-        if flows[unopen] == 0:
-            unopened.remove(unopen)
-
-    best = 0
-    for permute in itertools.permutations(unopened, 7):
-        current = "AA"
-        total_flow = 0
-        time = 0
-        my_permute = list(permute)
-        my_unopened = list(unopened)
-        while time < 30:
-            if current in my_unopened:
-                my_unopened.remove(current)
-                time = time + 1
-                total_flow = total_flow + (30 - time) * flows[current]
-            elif my_unopened and my_permute:
-                target = my_permute.pop()
-                distance = len(shortest_paths[current][target]) - 1
-                time = time + distance
-                current = target
-            else:
-                time = time + 1
-        best = max(best, total_flow)
-    print(best)
+    return valves, flows
 
 
 if __name__ == '__main__':
