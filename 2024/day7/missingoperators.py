85125b4d4f9c7bc0d5c199278a4725e5080bdf96
lcsmith <lcsmith2.0@gmail.com>
2024-12-12 20:53:42 -0600

    Solve day7 part2 easy peasy

--- 2024/day7/missingoperators.py
+++ 2024/day7/missingoperators.py
@@ -1,35 +1,30 @@
 import itertools
-import operator
 from collections import namedtuple
+
 
 OperationLine = namedtuple('OperationLine', ['expected_result', 'operands'])
 
-def run():
+def run(include_concat):
     parsed_lines = parse_input()
+
+    possible_operators = [lambda x,y: x+y, lambda x,y: x*y]
+    if include_concat:
+        possible_operators.append(lambda x,y: int(f'{x}{y}'))
 
     total_sum = 0
     for (expected_result, operands) in parsed_lines:
-        if is_possible(expected_result, operands):
+        if is_possible(expected_result, operands, possible_operators):
             total_sum += expected_result
 
     print(total_sum)
 
-def is_possible(expected_result, operands):
-    possible_operators = [operator.add, operator.mul]
 
+def is_possible(expected_result, operands, possible_operators):
     results = [int(operands[0])]
     for operand_idx in range (1, len(operands)):
         results = [op(val, int(operands[operand_idx])) for (val, op) in itertools.product(results, possible_operators)]
     return any([x == expected_result for x in results])
 
-# Implemented before I re-read the part about ignoring order of operations...
-def is_possible_respect_oo(expected_result, operands):
-    possible_operators = ['+', '*']
-
-    expressions = [operands[0]]
-    for operand_idx in range (1, len(operands)):
-        expressions = [exp + op + operands[operand_idx] for (exp, op) in itertools.product(expressions, possible_operators)]
-    return any([eval(x) == expected_result for x in expressions])
 
 def parse_input():
     with open('input') as infile:
@@ -37,5 +32,7 @@ def parse_input():
 
     return [OperationLine(int(line[0].strip(':')), line[1:]) for line in lines]
 
+
 if __name__ == '__main__':
-    run()
+    run(False)
+    run(True)
