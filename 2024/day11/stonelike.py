d02235e1c8ff390ebdc598c5032ecca4f35fde55
LCSmith <lcsmith2.0@gmail.com>
2024-12-16 21:11:22 -0600

    Optimize and solve day11 part 2

--- 2024/day11/stonelike.py
+++ 2024/day11/stonelike.py
@@ -1,31 +1,47 @@
 import math
+from collections import defaultdict
 
 
-def run():
+def run(num_generations):
     stones = parse_input()
 
-    # I know there's no way this will work in part 2, but let's do naive first.
-    for blinks in range(25):
-        stones = blink_all_stones(stones)
-    print(len(stones))
+    stone_frequencies = defaultdict(int)
+    for idx in range(len(stones)):
+        stone_frequencies[stones[idx]] += 1
 
-def blink_all_stones(stones):
-    blinked_list_of_lists = [blink_single_stone(stone)for stone in stones]
-    return [item for sublist in blinked_list_of_lists for item in sublist]
+    for blinks in range(num_generations):
+        stone_frequencies = blink_all_stone_frequencies(stone_frequencies)
+    total_stones = 0
+    for stone in stone_frequencies:
+        total_stones += stone_frequencies[stone]
+
+    print(total_stones)
+
+
+def blink_all_stone_frequencies(stone_frequencies):
+    new_frequencies = defaultdict(int)
+    for stone in stone_frequencies:
+        for new_stone in blink_single_stone(stone):
+            new_frequencies[new_stone] += stone_frequencies[stone]
+    return new_frequencies
 
 
 def blink_single_stone(number):
-    #If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+    # If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
     if number == 0:
         return [1]
 
-    # If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+    # If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
+    # The left half of the digits are engraved on the new left stone,
+    #   and the right half of the digits are engraved on the new right stone.
+    # (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
     digits = int(math.log10(number)) + 1
     if digits % 2 == 0:
         half_digits_mask = pow(10, digits / 2)
         return [int(number / half_digits_mask), int(number % half_digits_mask)]
 
-    # If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
+    # If none of the other rules apply, the stone is replaced by a new stone;
+    #   the old stone's number multiplied by 2024 is engraved on the new stone.
     return [number * 2024]
 
 
@@ -37,4 +53,5 @@ def parse_input():
 
 
 if __name__ == '__main__':
-    run()
+    run(25)
+    run(75)
