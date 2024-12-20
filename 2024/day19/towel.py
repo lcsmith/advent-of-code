bb1735cdaa66bc42a193b895581d8c985b77c816
lcsmith <lcsmith2.0@gmail.com>
2024-12-19 22:44:05 -0600

    Solve 19.2 by tracking num paths in

--- 2024/day19/towel.py
+++ 2024/day19/towel.py
@@ -1,23 +1,31 @@
+from collections import OrderedDict
 
 
 def run():
     towels, patterns = parse_input()
 
-    print(sum([1 for pattern in patterns if does_pattern_match(towels, pattern)]))
+    pattern_match_counts = [get_pattern_match_count(towels, pattern) for pattern in patterns]
+    print(sum([1 for x in pattern_match_counts if x > 0]))
+    print(sum(pattern_match_counts))
 
 
-def does_pattern_match(towels, pattern):
-    next_states = {pattern}
+def get_pattern_match_count(towels, pattern):
+    next_states = OrderedDict()
+    next_states[pattern] = 1
+    match_count = 0
     while any(next_states):
-        current_state = next_states.pop()
+        (current_state, num_paths_in) = next_states.popitem(last = False)
         for towel in towels:
             if current_state.startswith(towel):
                 consumed_state = current_state[len(towel):]
                 if consumed_state == "":
-                    return True
-                next_states.add(consumed_state)
-    return False
-
+                    match_count += num_paths_in
+                else:
+                    if consumed_state not in next_states:
+                        next_states[consumed_state] = num_paths_in
+                    else:
+                        next_states[consumed_state] += num_paths_in
+    return match_count
 
 
 def parse_input():
